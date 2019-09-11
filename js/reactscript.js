3493488b3d4f8061a6361a64a7bb1d060194a7d2
konstantin <oneuyooz@gmail.com>
2019-09-11 23:27:38 +0300

    Style

--- js/reactscript.js
+++ js/reactscript.js
@@ -5,6 +5,5 @@ var element = React.createElement(
   '\u041F\u0440\u0438\u0432\u0435\u0442'
 );
 var button_info = document.querySelector('.button-info');
-console.log(button_info);
 
 ReactDOM.render(element, container_block);
--- src/js/reactscript.js
+++ src/js/reactscript.js
@@ -1,7 +1,5 @@
 const container_block = document.getElementById('container');
 const element = <h1>Привет</h1>;
 const button_info = document.querySelector('.button-info');
-console.log(button_info);
-
 
 ReactDOM.render(element, container_block);
