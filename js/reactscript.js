83fb912d4d491d58052e92ffff7558f05c607461
konstantin <oneuyooz@gmail.com>
2019-09-11 22:46:02 +0300

    JSX | Babel

--- js/reactscript.js
+++ js/reactscript.js
@@ -1,8 +1,10 @@
-const container_block = document.getElementById('container');
-const element = <h1>Привет</h1>;
-const button_info = document.querySelector('.button-info');
+var container_block = document.getElementById('container');
+var element = React.createElement(
+  'h1',
+  null,
+  '\u041F\u0440\u0438\u0432\u0435\u0442'
+);
+var button_info = document.querySelector('.button-info');
+console.log(button_info);
 
-button_info.onclick = function () {
-    ReactDOM.render(element, container_block);
-}
-
+ReactDOM.render(element, container_block);
