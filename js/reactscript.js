cc4a25e5e797ddad7c85682a9406e4b1fcef3b35
konstantin <oneuyooz@gmail.com>
2019-09-11 22:00:17 +0300

    React | button

--- js/reactscript.js
+++ js/reactscript.js
@@ -1,4 +1,8 @@
 const container_block = document.getElementById('container');
 const element = <h1>Привет</h1>;
+const button_info = document.querySelector('.button-info');
 
-ReactDOM.render(element, container_block);
+button_info.onclick = function () {
+    ReactDOM.render(element, container_block);
+}
+
